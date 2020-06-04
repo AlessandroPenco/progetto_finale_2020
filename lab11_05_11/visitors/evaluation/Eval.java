510a09f5c5b14bfeea9c07eddf762c88d795f29c
AlessandroPenco <62288664+AlessandroPenco@users.noreply.github.com>
2020-06-04 21:07:47 +0200

    Add files via upload

---
+++ lab11_05_11/visitors/evaluation/Eval.java
@@ -0,0 +1,158 @@
+package lab11_05_11.visitors.evaluation;
+
+import java.io.PrintWriter;
+
+import lab11_05_11.environments.EnvironmentException;
+import lab11_05_11.environments.GenEnvironment;
+import lab11_05_11.parser.ast.Block;
+import lab11_05_11.parser.ast.Exp;
+import lab11_05_11.parser.ast.VarIdent;
+import lab11_05_11.parser.ast.Stmt;
+import lab11_05_11.parser.ast.StmtSeq;
+import lab11_05_11.visitors.Visitor;
+
+import static java.util.Objects.requireNonNull;
+
+public class Eval implements Visitor<Value> {
+
+	private final GenEnvironment<Value> env = new GenEnvironment<>();
+	private final PrintWriter printWriter; // output stream used to print values
+
+	public Eval() {
+		printWriter = new PrintWriter(System.out, true);
+	}
+
+	public Eval(PrintWriter printWriter) {
+		this.printWriter = requireNonNull(printWriter);
+	}
+
+	// dynamic semantics for programs; no value returned by the visitor
+
+	@Override
+	public Value visitProg(StmtSeq stmtSeq) {
+		try {
+			stmtSeq.accept(this);
+			// possible runtime errors
+			// EnvironmentException: undefined variable
+		} catch (EnvironmentException e) {
+			throw new EvaluatorException(e);
+		}
+		return null;
+	}
+
+	// dynamic semantics for statements; no value returned by the visitor
+
+	@Override
+	public Value visitAssignStmt(VarIdent ident, Exp exp) {
+		env.update(ident, exp.accept(this));
+		return null;
+	}
+
+	@Override
+	public Value visitPrintStmt(Exp exp) {
+		printWriter.println(exp.accept(this));
+		return null;
+	}
+
+	@Override
+	public Value visitVarStmt(VarIdent ident, Exp exp) {
+		env.dec(ident, exp.accept(this));
+		return null;
+	}
+
+	@Override
+	public Value visitIfStmt(Exp exp, Block thenBlock, Block elseBlock) {
+		if (exp.accept(this).toBool())
+			thenBlock.accept(this);
+		else if (elseBlock != null)
+			elseBlock.accept(this);
+		return null;
+	}
+
+	@Override
+	public Value visitBlock(StmtSeq stmtSeq) {
+		env.enterScope();
+		stmtSeq.accept(this);
+		env.exitScope();
+		return null;
+	}
+
+	// dynamic semantics for sequences of statements
+	// no value returned by the visitor
+
+	@Override
+	public Value visitSingleStmt(Stmt stmt) {
+		stmt.accept(this);
+		return null;
+	}
+
+	@Override
+	public Value visitMoreStmt(Stmt first, StmtSeq rest) {
+		first.accept(this);
+		rest.accept(this);
+		return null;
+	}
+
+	// dynamic semantics of expressions; a value is returned by the visitor
+
+	@Override
+	public Value visitAdd(Exp left, Exp right) {
+		return new IntValue(left.accept(this).toInt() + right.accept(this).toInt());
+	}
+
+	@Override
+	public Value visitIntLiteral(int value) {
+		return new IntValue(value);
+	}
+
+	@Override
+	public Value visitMul(Exp left, Exp right) {
+		return new IntValue(left.accept(this).toInt() * right.accept(this).toInt());
+	}
+
+	@Override
+	public Value visitSign(Exp exp) {
+		return new IntValue(-exp.accept(this).toInt());
+	}
+
+	@Override
+	public Value visitVarIdent(VarIdent id) {
+		return env.lookup(id);
+	}
+
+	@Override
+	public Value visitNot(Exp exp) {
+		return new BoolValue(!exp.accept(this).toBool());
+	}
+
+	@Override
+	public Value visitAnd(Exp left, Exp right) {
+		return new BoolValue(left.accept(this).toBool() && right.accept(this).toBool());
+	}
+
+	@Override
+	public Value visitBoolLiteral(boolean value) {
+		return new BoolValue(value);
+	}
+
+	@Override
+	public Value visitEq(Exp left, Exp right) {
+		return new BoolValue(left.accept(this).equals(right.accept(this)));
+	}
+
+	@Override
+	public Value visitPairLit(Exp left, Exp right) {
+		return new PairValue(left.accept(this), right.accept(this));
+	}
+
+	@Override
+	public Value visitFst(Exp exp) {
+		return exp.accept(this).toProd().getFstVal();
+	}
+
+	@Override
+	public Value visitSnd(Exp exp) {
+		return exp.accept(this).toProd().getSndVal();
+	}
+
+}
